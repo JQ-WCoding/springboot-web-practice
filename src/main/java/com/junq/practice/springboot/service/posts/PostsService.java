7e9c0f53b24e285b56f7ceb9730be2be69b9e45c
SpellBound <junlee36520620@gmail.com>
2021-04-24 23:59:49 +0900

    Day003
    Update, Response 추가
    해당 기능 test 완료
    local을 통해 해당 데이터 올바르게 insert쿼리를 통해 저장되는지 확인 완료

--- src/main/java/com/junq/practice/springboot/service/posts/PostsService.java
+++ src/main/java/com/junq/practice/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.junq.practice.springboot.service.posts;
 
+import com.junq.practice.springboot.domain.posts.Posts;
 import com.junq.practice.springboot.domain.posts.PostsRepository;
+import com.junq.practice.springboot.web.dto.PostsResponseDto;
 import com.junq.practice.springboot.web.dto.PostsSaveRequestDto;
+import com.junq.practice.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +19,24 @@ public class PostsService {
 
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
-        return postsRepository.save(requestDto.toEntity()).getId();
+        return postsRepository.save( requestDto.toEntity() ).getId();
+    }
+
+    @Transactional
+    // update 데이터베이스에 쿼리를 제거하는 부분이 없음
+    // JPA 영속성 컨텍스트 떄문 (엔티티를 영구 저장하는 환경)
+    // Entity 객체의 값만 변경하면 Update 쿼리가 필요없음 = 더티체킹
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        // lambda
+        Posts posts = postsRepository.findById( id ).orElseThrow( () -> new IllegalArgumentException( "해당 게시글이 없습니다. id =" + id ) );
+
+        posts.update( requestDto.getTitle(), requestDto.getContent() );
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById( id ).orElseThrow( () -> new IllegalArgumentException( "해당 게시글이 없습니다. id =" + id ) );
+        return new PostsResponseDto( entity );
     }
 }
